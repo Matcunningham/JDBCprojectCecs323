9cb5aa2ca8ce8f7038b81642c7b1b36a09f96f53
Mat Cunningham <matcunningham@ymail.com>
2018-03-13 11:08:30 -0700

    Change format

--- jdbcProject.sql
+++ jdbcProject.sql
@@ -1,44 +1,26 @@
 CREATE TABLE writingGroup (
-	groupName   VARCHAR(20) NOT NULL,
-	headWriter  VARCHAR(20),
+	groupName   VARCHAR(50) NOT NULL,
+	headWriter  VARCHAR(50),
 	yearFormed  SMALLINT,
-	subject     VARCHAR(20));
+	subject     VARCHAR(30),
+    CONSTRAINT group_pk PRIMARY KEY (groupName));
 	
 CREATE TABLE publisher (
-	pubName     VARCHAR(20) NOT NULL,
-	pubAddress  VARCHAR(20),
+	pubName     VARCHAR(50) NOT NULL,
+	pubAddress  VARCHAR(100),
 	pubPhone    VARCHAR(12),
-	pubEmail    VARCHAR(20));
+	pubEmail    VARCHAR(100),
+    CONSTRAINT pub_pk PRIMARY KEY (pubName));
 	
 CREATE TABLE book (
-	groupName       VARCHAR(20) NOT NULL,
-	pubName         VARCHAR(20) NOT NULL,
-	bookTitle       VARCHAR(40) NOT NULL,
+	groupName       VARCHAR(50) NOT NULL,
+	pubName         VARCHAR(50) NOT NULL,
+	bookTitle       VARCHAR(50) NOT NULL,
 	yearPublished   SMALLINT,
-	numberPages     SMALLINT);
-	
-
-ALTER TABLE publisher
-	ADD CONSTRAINT pub_pk
-	PRIMARY KEY (pubName);
-	
-ALTER TABLE writingGroup
-	ADD CONSTRAINT group_pk
-	PRIMARY KEY (groupName);
-	
-ALTER TABLE book
-	ADD CONSTRAINT book_pk
-	PRIMARY KEY (groupName, bookTitle);
-	
-ALTER TABLE book
-	ADD CONSTRAINT bookGroup_fk
-	FOREIGN KEY (groupName)
-	REFERENCES writingGroup (groupName);
-	
-ALTER TABLE book
-	ADD CONSTRAINT bookPub_fk
-	FOREIGN KEY (pubName)
-	REFERENCES publisher (pubName);
+	numberPages     SMALLINT,
+    CONSTRAINT book_pk PRIMARY KEY (groupName, bookTitle),
+    CONSTRAINT bookGroup_fk FOREIGN KEY (groupName) REFERENCES writingGroup (groupName),
+    CONSTRAINT bookPub_fk FOREIGN KEY (pubName) REFERENCES publisher (pubName));
 
 --DROP TABLE publisher;
 --DROP TABLE book;
